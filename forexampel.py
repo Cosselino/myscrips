9b222a349121f06cba11679b5f731b8d23f6e84f
Fredrik <frekar0621@skola.goteborg.se>
2022-09-08 14:41:39 +0200

    klar

---
+++ forexampel.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.8
+'''
+for i in range(0,10):
+   print(f'9*{i}={9*i}')
+'''
+
+for k in range(1,10):
+   print(f'\n{k}:ans tabell')
+   for i in range(0,10):
+      print(f'{k}*{i}={k*i}')
+
